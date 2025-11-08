1eedff3223e176377e66e67b46ab5d72313ee516
RedFoxWasHere <redfoxisnotred@gmail.com>
2025-11-08 17:51:13 +0545

    test

--- Codes.py
+++ Codes.py
@@ -1,5 +1,5 @@
 ### Test for Pushing the files in github 
 ### program to sort the given list in ascending order
 givenlist={2,1,3,42,23,4,63,12}
-in list enumerate(givenlist):
+for list enumerate(givenlist):
     
